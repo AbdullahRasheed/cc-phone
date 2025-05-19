2968502cbd5c12256e5f433eaac505f14b714d1e
AbdullahRasheed <ahrasheed2004@gmail.com>
2025-05-19 14:17:08 -0500

    Icon render fix (?)

--- normalboot.lua
+++ normalboot.lua
@@ -1,19 +1,32 @@
 local persist = require("persistence")
 local basalt = require("basalt")
 
-local defaultIco = loadfile("./ico_default")
+local file, err = fs.open("cc-phone/ico_default", "rb")
+if not file then error("Could not open file: " .. err) end
+
+local defaultIco = textutils.unserialize(file.readAll())
+file.close()
+
 if defaultIco == nil then
-    error("Could not load default icon")
+    error("Could not load default icon.. ill-formatted.")
 end
 
 local main = basalt.getMainFrame()
 
-for i, file in ipairs(fs.list("data/apps")) do
-    local filepath = "data/apps/" .. file .. "/metadata.lua"
+for i, filename in ipairs(fs.list("data/apps")) do
+    local filepath = "data/apps/" .. filename .. "/metadata.lua"
     local meta = persist.load_table(filepath)
+    local ico
 
-    local ico = loadfile(fs.combine("data/apps/" .. file, meta["ico"]))
-    main:addImage({bimg = ico or defaultIco, autoResize = true})
+    if meta["ico"] then
+        local f = fs.open(fs.combine("data/apps/" .. filename, meta["ico"]))
+        ico = textutils.unserialize(f.readAll())
+        f.close()
+    else
+        ico = defaultIco
+    end
+
+    main:addImage({bimg = ico, autoResize = true})
         :setSize(math.floor(main.width / 3), math.floor(main.height / 3))
         :setPosition(((i-1) % 3) * math.floor(main.width / 3), math.floor((i-1) / 3) * math.floor(main.height / 4) + 1)
 end
